fed8779de1584f007c237f0db5e0021f3ce02d59
Titus Wormer <tituswormer@gmail.com>
2021-10-27 09:39:24 +0200

    Update dev-dependencies

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 /**
  * Get the count of the longest repeating streak of `character` in `value`.
  *
- * @param {string} value Content.
- * @param {string} character Single character to look for
- * @returns {number} Count of most frequent adjacent `character`s in `value`
+ * @param {string} value
+ *   Content to search in.
+ * @param {string} character
+ *   Single character to look for.
+ * @returns {number}
+ *   Count of most frequent adjacent `character`s in `value`.
  */
 export function longestStreak(value, character) {
-  var source = String(value)
-  var index = source.indexOf(character)
-  var expected = index
-  var count = 0
-  var max = 0
+  const source = String(value)
+  let index = source.indexOf(character)
+  let expected = index
+  let count = 0
+  let max = 0
 
   if (typeof character !== 'string' || character.length !== 1) {
     throw new Error('Expected character')
