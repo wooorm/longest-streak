f9c98198b3df838b8be506f58fdb49b91fcd2b2b
Titus Wormer <tituswormer@gmail.com>
2022-11-15 12:54:41 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,11 +1,12 @@
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {longestStreak} from './index.js'
 
-test('longestStreak(value, character)', function (t) {
+test('longestStreak(value, character)', function () {
   // @ts-expect-error Incorrect `value`.
-  t.equal(longestStreak(true, 't'), 1, 'should coerce to string')
+  assert.equal(longestStreak(true, 't'), 1, 'should coerce to string')
 
-  t.throws(
+  assert.throws(
     function () {
       // @ts-expect-error Incorrect `character`.
       longestStreak('', 0)
@@ -14,7 +15,7 @@ test('longestStreak(value, character)', function (t) {
     'should throw when character is invalid (non-string)'
   )
 
-  t.throws(
+  assert.throws(
     function () {
       longestStreak('', 'incorrect')
     },
@@ -22,18 +23,16 @@ test('longestStreak(value, character)', function (t) {
     'should throw when character is invalid (too long string)'
   )
 
-  t.equal(longestStreak('', 'f'), 0, 'should work (1)')
-  t.equal(longestStreak('foo', 'o'), 2, 'should work (2)')
-  t.equal(longestStreak('fo foo fo', 'o'), 2, 'should work (3)')
-  t.equal(longestStreak('fo foo foo', 'o'), 2, 'should work (4)')
-  t.equal(longestStreak('fo fooo fo', 'o'), 3, 'should work (5)')
-  t.equal(longestStreak('fo fooo foo', 'o'), 3, 'should work (6)')
-  t.equal(longestStreak('ooo', 'o'), 3, 'should work (7)')
-  t.equal(longestStreak('fo fooo fooooo', 'o'), 5, 'should work (8)')
-  t.equal(longestStreak('fo fooooo fooo', 'o'), 5, 'should work (9)')
-  t.equal(longestStreak('fo fooooo fooooo', 'o'), 5, 'should work (10)')
+  assert.equal(longestStreak('', 'f'), 0, 'should work (1)')
+  assert.equal(longestStreak('foo', 'o'), 2, 'should work (2)')
+  assert.equal(longestStreak('fo foo fo', 'o'), 2, 'should work (3)')
+  assert.equal(longestStreak('fo foo foo', 'o'), 2, 'should work (4)')
+  assert.equal(longestStreak('fo fooo fo', 'o'), 3, 'should work (5)')
+  assert.equal(longestStreak('fo fooo foo', 'o'), 3, 'should work (6)')
+  assert.equal(longestStreak('ooo', 'o'), 3, 'should work (7)')
+  assert.equal(longestStreak('fo fooo fooooo', 'o'), 5, 'should work (8)')
+  assert.equal(longestStreak('fo fooooo fooo', 'o'), 5, 'should work (9)')
+  assert.equal(longestStreak('fo fooooo fooooo', 'o'), 5, 'should work (10)')
 
-  t.equal(longestStreak("'`'", '`'), 1, 'should match on one')
-
-  t.end()
+  assert.equal(longestStreak("'`'", '`'), 1, 'should match on one')
 })
